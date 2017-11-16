81549542f5d92f1effdaab6bf9467c2427288f36
Vinicius Santana <santanaviny@gmail.com>
2017-11-16 14:46:22 -0800

    Namespace

---
+++ namespace.js
@@ -0,0 +1,11 @@
+var store = window.store || {};
+
+if ( !store["basket"] ) {
+  store.basket = {};
+}
+if ( !store.basket["core"] ) {
+  store.basket.core = {};
+}
+store.basket.core = {
+// ...rest of our logic
+};
